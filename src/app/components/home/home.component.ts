b0d5043aae2106ec45800e70dccd9acca92fba30
Axel Segundo <axeljasegundo@gmail.com>
2020-11-09 09:47:07 -0600

    rutas, http, token, map

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.services';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: [
+  ]
+})
+export class HomeComponent  {
+
+  nuevasCanciones: any [] = [];
+
+  constructor( private spotify: SpotifyService ) {
+
+    this.spotify.getNewReleases()
+          .subscribe( (data:any) => {
+            this.nuevasCanciones = data;
+          });
+  }
+
+
+
+}
